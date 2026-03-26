0b7b476541b29a8360f59e3257187d53803bf47e
y10111 <15566449114@163.com>
2026-03-26 10:57:13 +0800

    移除不必要的组件与页面，仅保留项目相关

--- src/router/index.js
+++ src/router/index.js
@@ -10,16 +10,6 @@ const routes = [
     name: "dashboard",
     component: Dashboard,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
-    },
-  },
 ];
 
 const router = new VueRouter({
